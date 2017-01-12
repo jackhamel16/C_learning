7a7600da67dcc2e1be716a2bfada54adf253fb80
Jack Hamel <jackhaml16@gmail.com>
2017-01-12 11:31:25 -0500

    1-16 added, 17 and 18 not done

---
+++ chapter1/1-18.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+// Prints line and its length if it exceeds 80 characters
+
+int getline1(char s[], int lim)
+{
+        int c, i;
+
+        for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+                if (c!='\t' && s[i-1]==' ' && c==' ')
+			s[i] = c;
+		else
+			--i;
+        if (c == '\n') {
+                s[i] = c;
+                ++i;
+        }
+        s[i] = '\0';
+        return i;
+}
+
+void copy(char to[], char from[])
+{
+        int i;
+
+        i = 0;
+        while ((to[i] = from[i]) != '\0')
+                ++i;
+}
+
+main()
+{
+        int len;
+        int max;
+        char line[MAXLINE];
+        char longest[MAXLINE];
+
+        max = 0;
+        while ((len = getline1(line, MAXLINE)) > 0)
+                if (len > max) {
+                        max = len;
+                        copy(longest, line);
+                }
+        if (max > 0)
+                printf("%s\n%d", longest,max);
+        return 0;
+}
+
